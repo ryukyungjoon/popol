8f7c8412e5ba490c4209b26a078f56f820dda394
unknown <ryuryukj6663@gmail.com>
2021-03-29 15:59:32 +0900

    CICIDS code Remark

--- Deep-Learning_Project/CICIDS 2018/CICIDS_IDS.py
+++ Deep-Learning_Project/CICIDS 2018/CICIDS_IDS.py
@@ -136,6 +136,7 @@ class Preprocessing:
     def __init__(self):
         pass
 
+    ''' Data Normalization '''
     def normalization(self, train_x, train_y, test_x, test_y, norm_type):
         if norm_type == 'mms':
             self.norm_tool = MinMaxScaler(feature_range=(0, 255))
@@ -152,16 +153,17 @@ class Preprocessing:
 
         return norm_trainx, norm_trainy, norm_testx, norm_testy
 
+    ''' Feature Selection '''
     def feature_selection(self, train_x, train_y, test_x, test_y, fs_type):
+        ''' data type에 따른 분류 (number, object)'''
         num_col = train_x.select_dtypes('number').columns
         obj_col = train_x.select_dtypes('object').columns
 
         train_num, train_obj = train_x[num_col], train_x[obj_col]
         test_num, test_obj = test_x[num_col], test_x[obj_col]
 
+        ''' No.0 is HYBRID FEATURE SELECTION '''
         if fs_type == 0:
-            ''' No.0 is HYBRID FEATURE SELECTION '''
-
             """ WRAPPER METHOD """
             fs_model = RandomForestClassifier(random_state=0, n_estimators=10, n_jobs=-1)
             fs_model.fit(train_num, train_y)
@@ -208,7 +210,7 @@ class Preprocessing:
             print('No Selected FEATURE SELECTION TYPE')
             return 0
 
-
+''' CICIDS 2018 Dataset Preprocessing '''
 class CIC_Transform(Preprocessing):
     def __init__(self, data, label='Label', norm_type=None, fs_type=None):
         self.y_label = label
@@ -218,6 +220,7 @@ class CIC_Transform(Preprocessing):
         self.train_x, self.train_y = None, None
         self.test_x, self.test_y = None, None
 
+''' Data Clearning '''
     def cic_cleaning(self):
         print("Data Cleaning...")
         data1 = self.data.dropna(axis=0, how='any')
@@ -229,6 +232,7 @@ class CIC_Transform(Preprocessing):
 
         return x, y
 
+''' Train & Test Split '''
     def cic_split(self):
         print("[train_set] <=> [test_set]")
 
@@ -237,6 +241,7 @@ class CIC_Transform(Preprocessing):
         self.train_y, self.train_x = train_set['Label'], train_set.drop('Label', 1)
         self.test_y, self.test_x = test_set['Label'], test_set.drop('Label', 1)
 
+''' Imbalance Data  '''
     def balancing(self):
         rus = RandomUnderSampler(sampling_strategy=down_dic, random_state=0)
         sm = SMOTE(kind='regular', sampling_strategy=up_dic, random_state=0)
@@ -252,16 +257,11 @@ class CIC_Transform(Preprocessing):
 
     def data_transform(self, save=None):
         x, y = self.cic_cleaning()
-
         classes_y = np.unique([y])
-
         self.cic_split()
-
         train_x, train_y, test_x, test_y = self.normalization(self.train_x, self.train_y,
                                                               self.test_x, self.test_y, norm_type=self.norm_type)
-
         train_x, test_x = self.feature_selection(train_x, train_y, test_x, test_y, self.fs_type)
-
         train_x, train_y = self.balancing()
 
         if save:
@@ -275,15 +275,18 @@ class Traninng:
         self.optimizer = Adam()
         self.epochs = epoch
 
-
-
         if model_name == 'dnn':
             self.dnn(train_x, train_y, test_x, test_y)
         if model_name == 'mlp':
             self.mlp(train_x, train_y, test_x, test_y, mini_batch_size=batch_size)
 
-
     def dnn(self, train_x, train_y, test_x, test_y):
+        self.model = Sequential()
+        self.model.add(Dense(128, activation='relu', input_shape=(78, 1), name="Input_Layer"))
+        self.model.add(Dense(64, activation='relu'))
+        self.model.add(Dense(32, activation='relu'))
+        self.model.add(Dense(16, activation='relu'))
+        self.model.add(Dense(15, activation='softmax'))
 
     def mlp(self, train_x, train_y, test_x, test_y, mini_batch_size):
         self.model = MLPClassifier(hidden_layer_sizes=(1000, 500, 100), max_iter=100, random_state=42)
@@ -293,9 +296,9 @@ class Traninng:
         start = time.time()
         for i in range(1, total_epoch):
             end_batch = i * mini_batch_size
-            use_model.partial_fit(train_x[current_batch:end_batch], train_y[current_batch:end_batch], classes=classes_y)
+            self.model.partial_fit(train_x[current_batch:end_batch], train_y[current_batch:end_batch], classes=classes_y)
             current_batch = end_batch
-        use_model.partial_fit(train_x[current_batch:batch_size], train_y[current_batch:batch_size], classes=classes_y)
+        self.model.partial_fit(train_x[current_batch:batch_size], train_y[current_batch:batch_size], classes=classes_y)
         end = time.time()
 
 
@@ -318,10 +321,7 @@ if __name__ == "__main__":
     """
     Training
     """
-    train = Traninng(train_x, trina_y, test_x, test_y, batch_size, epoch, model_name)
-
-
-
+    train = Training(train_x, trina_y, test_x, test_y, batch_size, epoch, model_name)
 
     raw_encoded, raw_cat = data_Label.factorize()
     print(raw_encoded, raw_cat)
@@ -329,7 +329,10 @@ if __name__ == "__main__":
 
     y_class = np.unique([re])
     print(y_class)
-    dw.print_confusion_matrix(use_model_confusion_matrix, classes_y)
 
+    """
+    print_confusion_matrix & classification_report
+    """
+    dw.print_confusion_matrix(use_model_confusion_matrix, classes_y)
     report = classification_report(y_test, pred, labels=y_class, target_names=classes_y)
     print(str(report))
